c2346b9994e37e3931de16b079871f38e9f83296
Sarthak Jain <sarthakjain4452@gmail.com>
2026-01-14 22:53:25 +0530

    included .env file

--- src/config.py
+++ src/config.py
@@ -1,2 +1,10 @@
-SPREADSHEET_ID = "1-F-84fYevlEFgJ7BdhETR_CJhXQvydsn97bPZpkCgog"
-SHEET_NAME = "Sheet1"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+SHEET_NAME = os.getenv("SHEET_NAME", "Sheet1")
+
+if not SPREADSHEET_ID:
+    raise ValueError("SPREADSHEET_ID not found in environment variables")
